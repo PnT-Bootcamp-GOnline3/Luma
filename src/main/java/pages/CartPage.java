b349a03b1fd385123af0fab56b4780ba6fb60a88
Yrassim <yrassim@gmail.com>
2022-09-14 02:02:43 -0400

    Added the file where to save the cart

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -3,6 +3,8 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 import static browser.Browser.driver;
@@ -12,19 +14,29 @@ public class CartPage {
 
     List<WebElement> items = driver.findElements(By.cssSelector("strong[class='product-item-name']"));
 
+    //I wanted to add quantity to the file(we will do it if we have time)
+    List<WebElement> qty = driver.findElements(By.cssSelector("input[class='input-text qty']"));
+
+
     public CartPage fileSave(){
-        
-        String[] array = new String[items.size()-2];
-        
-        int i = 2;
-        for (WebElement row : items) {                
-            array[i] = row.getText();
-            i++;
-        }
-        for(int j = 0; i<items.size()-1; j++) {
-            System.out.println(array[j]);
+
+        //System.out.println();
+
+        try {
+            FileWriter myWriter = new FileWriter("src/main/java/myFiles/filename.txt");
+
+            for(int i=2; i< items.size(); i++) {
+                myWriter.write(items.get(i).getText() + "\t" + "with Quantity: " + qty.get(i - 2).getAttribute("value"));
+                myWriter.write(" " + "\n");
+            }
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
         return  this;
     }
 
 }
+
